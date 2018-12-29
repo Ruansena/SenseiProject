40602ba91a179786104128cb0e8508c2c7dc021d
Kise <ruosena@gmail.com>
2018-12-29 18:59:51 -0200

    refactoring

--- WebScraping/atchDownloader.js
+++ WebScraping/atchDownloader.js
@@ -1,9 +1,9 @@
 const r = require('request');
 const fs = require('fs');
 
-const saveAtchInFolder = function(author, name, url, title) {
+const saveAtchInFolder = function(path, url, title) {
     console.log(`Saving ${title} image...`)
-    r.get(url).pipe(fs.createWriteStream(`./${author}/${name}`));
+    r.get(url).pipe(fs.createWriteStream(path));
 }
 
 module.exports = saveAtchInFolder;
