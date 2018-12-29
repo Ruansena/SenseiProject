097976148089f88d9ff867c1d1dda5c627735b0f
Kise <ruosena@gmail.com>
2018-12-29 14:14:13 -0200

    testing module

---
+++ WebScraping/getUrl.js
@@ -0,0 +1,5 @@
+const UriParse = require('./imgUriParse');
+const url = 'https://dribbble.com/shots/3789253-Girl-Hacker-Tee-Shirt-Design-Pair-Programming-on-iOS-for-Swift';
+// const url = 'https://dribbble.com/shots/5746960-Room-design-page';
+
+UriParse(url);
---
+++ WebScraping/saveImg.js
@@ -0,0 +1,4 @@
+const buff = require('./imgBuffer');
+const url = 'https://i3-cdn.pokyun.tv/215x290/100/r/www.pokyun.tv/storage/uploads/389af5168a6a508eb98f4c8c13a550d0-2.jpg';
+
+buff('SOUSEI-NO-ONMYOUJI.jpg', url);
