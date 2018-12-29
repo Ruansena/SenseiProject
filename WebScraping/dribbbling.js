eab65814c3b3ee85cae12bdd9d6d3f1c326649d9
Kise <ruosena@gmail.com>
2018-12-29 19:00:27 -0200

    corrections and improvements

--- WebScraping/dribbbling.js
+++ WebScraping/dribbbling.js
@@ -6,9 +6,14 @@ const crawAtchUrl = require('./crawUrl');
 const downloadAtch = require('./atchDownloader');
 const d = new Date;
 
+function makeFolder(dir) {
+    if(!fs.existsSync(dir)) fs.mkdirSync(dir) //Somente se não exisitir a pasta
+    return dir;
+}
+
 function writeData(name, data, folder) {
     if (folder) {
-        if(!fs.existsSync(folder)) fs.mkdir(folder) //Se não exisitir a pasta, crie
+        makeFolder(folder);
         fs.writeFile(`${folder}/${name}`, data, err => {
             console.log(err || 'Success Writing!');
         })        
@@ -20,20 +25,23 @@ function writeData(name, data, folder) {
     }
 }
 
+console.log('Fetching shots from dribbble...')
 pupp.launch()
 .then(browser => browser.newPage())
 .then(page => { 
     return page.goto(url).then(() => {
         return page.$$eval('.dribbbles > li', nodes => { //pega todos nós de shots
-            return nodes.map(shot => { //mapea os dados de cada shot
-                let shotHref = shot.querySelector('.dribbble-link').getAttribute('href');
+            return nodes.map(shot => { //mapeia os dados de cada shot
+                let href = shot.querySelector('.dribbble-link').getAttribute('href');
+                let AtchHref = shot.querySelector('.dribbble-link img').getAttribute('src');
+                AtchHref = `${AtchHref.slice(0, AtchHref.lastIndexOf('_'))}${AtchHref.slice(AtchHref.lastIndexOf('.'))}`;
                 return {
                     author: shot.querySelector('a[rel=contact]').textContent.trim(),
                     title: shot.querySelector('strong').innerHTML,
-                    href: shotHref,
-                    AtchName: shotHref.slice(shotHref.lastIndexOf('/')+1),
-                    AtchSrc: '',
-                    AtchHref: '',
+                    href,
+                    AtchName: AtchHref.slice(AtchHref.lastIndexOf('/')+1),
+                    AtchSrc: `./${AtchHref.slice(AtchHref.lastIndexOf('/')+1)}`,
+                    AtchHref,
                 }
             })
         })
@@ -44,29 +52,35 @@ pupp.launch()
     writeData(`dribbbleShots${d.getMonth()}-${d.getDate()}.json`, JSON.stringify(shots), `./${'DailyShots'}`);
     return shots;
 })
-.then(shots => { //Acessa página do shot e guarda src do attachment
-    Promise.all(
-        shots.forEach(async shot => {
-            await crawAtchUrl(shot.title, `${mainUrl}${shot.href}`)
-                .then(result => {
-                    shot.AtchHref = result;
-                    shot.AtchSrc = `./${shot.AtchName}${result.slice(result.lastIndexOf('.'))}`
-                }
-            )
-        })
-    )
-    return shots;
-})
-.then(shots => { //Acessa cada attachment e salva
+// .then(async shots => { //Acessa página do shot e guarda src do attachment (Futuramente para salvar todos attachments do shot)
+//     return await shots.map(async shot => {
+//         await crawAtchUrl(shot.title, `${mainUrl}${shot.href}`)
+//             .then(result => {
+//                 shot.AtchHref = result;
+//                 shot.AtchSrc = `./${shot.AtchName}${result.slice(result.lastIndexOf('.'))}`
+//             }
+//         )
+//     })
+    // return Promise.all(
+    //     shots.forEach(async shot => {
+    //         await crawAtchUrl(shot.title, `${mainUrl}${shot.href}`)
+    //             .then(result => {
+    //                 shot.AtchHref = result;
+    //                 shot.AtchSrc = `./${shot.AtchName}${result.slice(result.lastIndexOf('.'))}`
+    //             }
+    //         )
+    //     })
+    // );
+// })
+.then(shots => { //Acessa o attachment principal e salva
         Promise.all(
             shots.forEach(async shot => {
-                await downloadAtch(shot.author, shot.AtchName, shot.AtchHref, shot.title);
+                await downloadAtch(`./${makeFolder(shot.author)}/${shot.AtchName}`, shot.AtchHref, shot.title);
             })
         )
-    return shots;
 })
-.then(shots => { //Atualiza os dados
-    console.log('Rewriting shots data');
-    writeData(`dribbbleShots${d.getMonth()}-${d.getDate()}.json`, JSON.stringify(shots), `./${'DailyShots'}`);
-})
+// .then(shots => { //Atualiza os dados, futuramente para salvar mais de um attachment
+//     console.log('Rewriting shots data');
+//     writeData(`dribbbleShots${d.getMonth()}-${d.getDate()}.json`, JSON.stringify(shots, undefined, 2), `./${'DailyShots'}`);
+// })
 .catch(err => console.log("Error: ", err))
